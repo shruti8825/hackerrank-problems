ed5482a92870f746bc7aeba7620d3780fa8d1e8c
shruti <shrutijangalgi2002@gmail.com>
2020-05-23 21:04:12 +0530

    cpp basic data types

---
+++ practice/cpp/intro/basic_data_types.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdio>
+#include<math.h>
+ //MAX =-10;
+using namespace std;
+
+int main() {
+   int a;
+   long l;
+   char c;
+   float ff =-ff;
+   double d =-d;
+   cin>>a>>l>>c>>ff>>d;
+   //cin>>l;
+   //cin>>c;
+   //cin>>f;
+  // cin>>d;
+   cout<<a<<"\n";
+   cout<<l<<"\n";
+   cout<<c<<"\n";
+   cout<<ff<<"\n";
+   cout<<d<<"\n";
+    return 0;
+}
